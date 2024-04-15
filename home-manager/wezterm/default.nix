a124234900d9a6134cfac17c0c5ef2042ccfd3c2
blank71 <8blank71@gmail.com>
2024-04-15 19:54:43 +0900

    add wayland_gnome.lua

--- home-manager/wezterm/default.nix
+++ home-manager/wezterm/default.nix
@@ -2,8 +2,13 @@
   pkgs,
   ...
 }: {
-  home.file.".wezterm.lua" = {
-    source = ./wezterm.lua;
+  home.file = {
+    ".wezterm.lua" = {
+      source = ./wezterm.lua;
+    };
+    ".config/wezterm/wayland_gnome.lua" = {
+      source = ./wayland_gnome.lua;
+    };
   };
   home.packages = with pkgs;  [
     wezterm

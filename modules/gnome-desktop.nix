3f8913db2175f54c25cb59f7c573d610e3736a38
blank71 <8blank71@gmail.com>
2024-09-11 09:14:26 +0900

    fix

--- modules/gnome-desktop.nix
+++ modules/gnome-desktop.nix
@@ -5,21 +5,10 @@
 }: {
   users.users."${username}".packages = with pkgs; [
     gnome-extension-manager
-<<<<<<< HEAD
-    gnome.pomodoro
-    # gnome-pomodoro
-    gnomeExtensions.keyman
-    # gnome-keyring
-    gnome.gnome-tweaks
-    # gnome-tweaks
-    gnome.gnome-sound-recorder
-    #gnomeExtensions.gtk-title-bar
-=======
     gnome-pomodoro
     gnome-keyring
     gnome-tweaks
     gnome-sound-recorder
->>>>>>> origin/main
     gnomeExtensions.appindicator
     gnomeExtensions.just-perfection
     gnomeExtensions.keyman

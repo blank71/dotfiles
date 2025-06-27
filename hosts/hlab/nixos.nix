bf1975453f9b341e6b7b7f5bcd864a9f0c2e2c03
blank71 <8blank71@gmail.com>
2025-06-27 09:50:47 +0900

    add: gnome-remote-desktop

--- hosts/hlab/nixos.nix
+++ hosts/hlab/nixos.nix
@@ -89,19 +89,25 @@
 
   # xrdp
   services.gnome.gnome-remote-desktop.enable = true;
-  services.xrdp = {
-    audio.enable = true;
-    enable = true;
-    defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-    port = 3389;
-    openFirewall = true;
-  };
+  # services.xrdp = {
+  #   audio.enable = true;
+  #   enable = true;
+  #   defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+  #   port = 3389;
+  #   openFirewall = true;
+  # };
+  #
+  # networking.firewall = {
+  #   enable = true;
+  #   allowedUDPPorts = [3389];
+  #   allowedTCPPorts = [3389];
+  # };
 
-  networking.firewall = {
-    enable = true;
-    allowedUDPPorts = [3389];
-    allowedTCPPorts = [3389];
-  };
+  # gnome-remote-desktop
+  # services.xrdp.enable = true;
+  # services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+  # services.xrdp.openFirewall = true;
+ 
 
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;

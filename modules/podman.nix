6c5ecb3e9d3b487daf409838ca88aa8dbcaa4628
blank71 <8blank71@gmail.com>
2025-07-11 16:52:16 +0900

    enable doker

--- modules/podman.nix
+++ modules/podman.nix
@@ -1,16 +1,22 @@
 { 
   pkgs, 
+  username,
   ...
 }: {
   virtualisation = {
+    containers.registries.search = ["docker.io"];
     podman = {
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      dockerCompat = false;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
+    docker = {
+      enable = true;
+    };
   };
+  users.users."${username}".extraGroups = [ "docker" ];
 }

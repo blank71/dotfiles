0d7e355a48ea1aede9695d0552186b3ffc0d6a76
blank71 <8blank71@gmail.com>
2025-07-27 07:23:55 +0900

    fix: zsh

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -1,21 +1,22 @@
 {pkgs, ...}: {
-  home.file.".zshrc" = {
-    text = ''
-    # Lines configured by zsh-newuser-install
-    setopt autocd extendedglob nomatch notify
-    unsetopt beep
-    bindkey -e
-    bindkey '^[[1;5C' forward-word
-    bindkey '^[[1;5D' backward-word
-    bindkey "^[[3~" delete-char
-    bindkey "^H" backward-delete-char
+  programs.zsh = {
+    enable = true;
+    initContent = ''
+      # Lines configured by zsh-newuser-install
+      setopt autocd extendedglob nomatch notify
+      unsetopt beep
+      bindkey -e
+      bindkey '^[[1;5C' forward-word
+      bindkey '^[[1;5D' backward-word
+      bindkey "^[[3~" delete-char
+      bindkey "^H" backward-delete-char
 
-    # End of lines configured by zsh-newuser-install
-    # The following lines were added by compinstall
-    zstyle :compinstall filename '/home/bl/.zshrc'
+      # End of lines configured by zsh-newuser-install
+      # The following lines were added by compinstall
+      zstyle :compinstall filename '/home/bl/.zshrc'
 
-    autoload -Uz compinit
-    compinit
+      autoload -Uz compinit
+      compinit
     '';
   };
 }

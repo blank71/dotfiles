46e23667f930722929541bf3724155d779f177a8
blank71 <8blank71@gmail.com>
2025-08-06 10:29:17 +0900

    Revert "i18n 23.11"
    This reverts commit 6958363bfb4a9f3b1942fee7803e4f41c503a954.

--- modules/i18n-en.nix
+++ modules/i18n-en.nix
@@ -1,32 +1,8 @@
-{
+{ 
   config,
   pkgs,
   ...
-}:
-
-let
-  # Import nixpkgs from nixos-24.05 for all i18n-related packages
-  i18nPkgs = import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-24.05-i18n";
-    url = "https://github.com/NixOS/nixpkgs/";
-    ref = "refs/heads/nixos-23.11";
-    rev = "7ad2e9c9fedb5c7f4e5edeb655b385228eba6193";
-  }) { system = pkgs.system; };
-in
-
-{
-  # Use nixos-24.05 packages for i18n via overlay
-  nixpkgs.overlays = [
-    (final: prev: {
-      # Override i18n-related packages with versions from nixos-24.05
-      fcitx5 = i18nPkgs.fcitx5;
-      fcitx5-mozc = i18nPkgs.fcitx5-mozc;
-      fcitx5-gtk = i18nPkgs.fcitx5-gtk;
-      fcitx5-qt = i18nPkgs.fcitx5-qt;
-      fcitx5-with-addons = i18nPkgs.fcitx5-with-addons;
-    })
-  ];
+}: {
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -44,9 +20,8 @@ in
 
   i18n.inputMethod = {
     #enabled = "fcitx5";
-    fcitx5.waylandFrontend = true;
     type = "fcitx5";
     enable = true;
-    fcitx5.addons = [ pkgs.fcitx5-mozc ];
+    fcitx5.addons = [pkgs.fcitx5-mozc];
   };
 }
